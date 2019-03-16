7579204e96c19456946c23c34659c5ff1c48c31f
Slava <slavazhil@gmail.com>
2019-03-16 16:32:44 -0700

    commit

--- main.js
+++ main.js
@@ -39,24 +39,24 @@ function closeAddListing() {
 }
 
 document.getElementById('uploadFile').addEventListener('change', function() {
-	let file;
-	let destination = document.getElementById('addListingPicture');
-	destination.src = '';
+	var file;
+	var destination = document.getElementById('addListingPicture');
 
 	// Looping in case they uploaded multiple files
-	for(let x = 0, xlen = this.files.length; x < xlen; x++) {
+	for(var x = 0, xlen = this.files.length; x < xlen; x++) {
 		file = this.files[x];
 		if(file.type.indexOf('image') != -1) { // Very primitive "validation"
 
-			let reader = new FileReader();
+			var reader = new FileReader();
 
 			reader.onload = function(e) {
-				let img = new Image();
+				var img = new Image();
 				img.src = e.target.result; // File contents here
 
-				destination.appendChild(img);
+				destination.src = "" + e.target.result;
 			};
-			
+			console.log(reader.readAsDataURL(file));
+            
 			reader.readAsDataURL(file);
 		}
 	}
