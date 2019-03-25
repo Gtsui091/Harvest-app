11473b205def55cc4ecac14c82d67e2edca99679
Slava <slavazhil@gmail.com>
2019-03-24 19:08:57 -0700

    add info modal

--- main.js
+++ main.js
@@ -102,20 +102,43 @@ document.getElementById("add-listing-form").addEventListener("submit", function(
     reader.readAsDataURL(form.elements[0].files[0]);
 });
 
-// Pull all listings on page load + Add new listings
+// Pull all listings on page-load + Add new listings
 firebase.database().ref("Listings").on('child_added', function(listing) {    
     addListingToPage(listing);
 });
 
 function addListingToPage(listing) {
     document.getElementById("listings").innerHTML += `
-        <div class="listing" id="${listing.key}">
+        <div class="listing" id="${listing.key}" onclick="displayInfoModal(this.id)">
             <div class="listing-image" style="background-image: url(${listing.val().image});"></div>
             <div class="listing-name">${listing.val().name}</div>
             <div class="listing-city">${listing.val().city}</div>
             <div class="listing-weight">${listing.val().weight} lb</div>
         </div>
     `;
+}
+
+function displayInfoModal(key) {
+    let id = "IgkRb7HWIMfpA17mSzXjQTpiNa92"; // FIX ME
+
+    document.getElementById("info").style.display = "flex";
+
+    firebase.database().ref('Listings/' + key).once('value').then(function(listing) {
+        console.log(listing.val());
+        document.getElementById("info-name").innerHTML = listing.val().name;
+        document.getElementById("info-image").style.backgroundImage = "url("+listing.val().image+")";
+        document.getElementById("info-city").innerHTML = listing.val().city;
+        document.getElementById("info-weight").innerHTML = listing.val().weight;
+    });
+
+    firebase.database().ref('Users/' + id).once('value').then(function(user) {
+        document.getElementById("info-email").innerHTML = user.val().email;
+        document.getElementById("info-phone").innerHTML = user.val().phoneNumber;
+    });
+}
+
+function hideInfoModal() {
+    document.getElementById("info").style.display = "none";
 }
 
 // Outgoing Trade Request Modal
