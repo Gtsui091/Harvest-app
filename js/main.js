e883df9dec2f595fe0600d45aa54a0db934f0427
Slava <slavazhil@gmail.com>
2019-04-01 12:21:31 -0700

    edit js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var config = {
 firebase.initializeApp(config);
 var page = window.location.pathname.split("/").pop();
 var userID;
-
+console.log(page);
 firebase.auth().onAuthStateChanged(function(user){
     console.log(page);
     
